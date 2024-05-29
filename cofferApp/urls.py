6014f6cac898361191013ccf56d1c81b97d85792
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-05-29 22:13:35 +0100

    frontend commit

--- cofferApp/urls.py
+++ cofferApp/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from cofferApp import views
+from .views import login_view, register_user
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.index, name='index'),
+     path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
