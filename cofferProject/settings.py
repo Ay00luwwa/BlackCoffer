09088d3128930759222b758c6fa1cef2ae210e6e
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-07-02 06:22:10 +0100

    commit deployment2

--- cofferProject/settings.py
+++ cofferProject/settings.py
@@ -73,17 +73,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "cofferProject.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "CoffersDB",
-        "USER": "moni",
-        "PASSWORD":"moni",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
