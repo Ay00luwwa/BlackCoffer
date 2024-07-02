de692697c27ff2e6c1889802cb07ed8a0fd08b85
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-07-02 06:15:01 +0100

    commit deploy

--- cofferApp/views.py
+++ cofferApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
 from rest_framework.views import APIView
--- cofferProject/wsgi.py
+++ cofferProject/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cofferProject.settings")
 
 application = get_wsgi_application()
+
+# vercel cofiguration
+
+app = application
